fab7da9088594b41b751ff72f941a361b9c22632
dianerdiana <dianerdiana.de@gmail.com>
2026-01-26 17:47:10 +0700

    Add entities and mappers

---
+++ src/modules/album/domain/entities/album.entity.ts
@@ -0,0 +1,32 @@
+import { v7 as uuidv7 } from 'uuid';
+
+export interface Album {
+  id: string;
+  name: string;
+  year: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export class AlbumEntity implements Album {
+  id: string;
+  name: string;
+  year: number;
+  createdAt: string;
+  updatedAt: string;
+
+  constructor({ id, name, year, createdAt, updatedAt }: Album) {
+    this.id = id;
+    this.name = name;
+    this.year = year;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  static create(payload: Omit<Album, 'id' | 'createdAt' | 'updatedAt'>) {
+    const timestamp = new Date().toISOString();
+    const id = uuidv7();
+
+    return new AlbumEntity({ ...payload, id, createdAt: timestamp, updatedAt: timestamp });
+  }
+}
