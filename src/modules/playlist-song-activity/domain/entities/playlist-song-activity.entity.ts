8f199f5e6e8cfbd5aeee4579ebef01a36b8c2a25
dianerdiana <dianerdiana.de@gmail.com>
2026-01-27 23:03:02 +0700

    Add core playlist-song-activity

---
+++ src/modules/playlist-song-activity/domain/entities/playlist-song-activity.entity.ts
@@ -0,0 +1,47 @@
+import { v7 as uuidv7 } from 'uuid';
+
+export interface PlaylistSongActivity {
+  id: string;
+  playlistId: string;
+  songId: string;
+  userId: string;
+  action: string;
+  time: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export class PlaylistSongActivityEntity implements PlaylistSongActivity {
+  id: string;
+  playlistId: string;
+  songId: string;
+  userId: string;
+  action: string;
+  time: string;
+  createdAt: string;
+  updatedAt: string;
+
+  constructor(playlistSong: PlaylistSongActivity) {
+    const { id, playlistId, songId, userId, action, time, createdAt, updatedAt } = playlistSong;
+
+    this.id = id;
+    this.playlistId = playlistId;
+    this.songId = songId;
+    this.userId = userId;
+    this.action = action;
+    this.time = time;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  static create(payload: Omit<PlaylistSongActivity, 'id' | 'createdAt' | 'updatedAt'>) {
+    const timestamp = new Date().toISOString();
+    return new PlaylistSongActivityEntity({
+      ...payload,
+      id: uuidv7(),
+      time: timestamp,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    });
+  }
+}
